acfb311d49af39c0d2afa358fd7e59da6b83b548
Abhishek Rudra <a.rudra@mail.utoronto.ca>
2015-03-02 19:49:51 -0500

    adding costs function

--- DroidCrypt/app/src/main/java/com/droidcrypt/Mat2D.java
+++ DroidCrypt/app/src/main/java/com/droidcrypt/Mat2D.java
@@ -1,9 +1,7 @@
 package com.droidcrypt;
 
 import android.graphics.Bitmap;
-
 import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * Created by Abhishek on 2/23/2015.
@@ -15,14 +13,14 @@ public class Mat2D
 {
     public int rows;
     public int cols;
-    private ArrayList<Integer> vector;
+    //private ArrayList<Integer> vector;
     public Bitmap image;
 
     public Mat2D (int rows, int cols, Bitmap input)
     {
         this.rows = rows;
         this.cols = cols;
-        vector = new ArrayList<Integer>(rows*cols);
+        //vector = new ArrayList<Integer>(rows*cols);
         if (image == null) {
             Bitmap.Config config = Bitmap.Config.ARGB_8888;
             image = Bitmap.createBitmap(rows, cols, config);
@@ -44,5 +42,32 @@ public class Mat2D
 //    {
 //        Collections.shuffle(vector);
 //    }
+
+    static Mat2D Padding_Mirror(Mat2D mat, int padSizeRows, int padSizeCols)
+    {
+        Mat2D result = new Mat2D(mat.rows + (2*padSizeRows), mat.cols + (2*padSizeCols), null);
+        for (int row=0; row < result.rows; row++)
+        {
+            int rowOrig;
+            if (row < padSizeRows)
+                rowOrig = padSizeRows - row - 1;
+            else if (row > mat.rows + padSizeRows - 1)
+                rowOrig = 2*(mat.rows) + padSizeRows - 1 - row;
+            else rowOrig = row - padSizeRows;
+
+            for (int col=0; col < result.cols; col++)
+            {
+                int colOrig;
+                if (col < padSizeCols)
+                    colOrig = padSizeCols - col - 1;
+                else if (col > mat.cols + padSizeCols - 1)
+                    colOrig = 2*(mat.cols) + padSizeCols - 1 - col;
+                else colOrig = col - padSizeCols;
+
+                result.Write(row, col, mat.Read(rowOrig, colOrig));
+            }
+        }
+        return result;
+    }
 
 }
